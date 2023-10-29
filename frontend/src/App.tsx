26f3e3151dc54e1cebda4747166c05837e556c0b
jack do <jackie.duc.do@gmail.com>
2023-10-30 00:02:40 +1100

    add react speech recognition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import "./App.css";
+import microPhoneIcon from "./assets/microphone.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [isListening, setIsListening] = useState(false);
+  const microphoneRef = useRef<HTMLDivElement>(null);
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return <div className="mircophone-container">Browser is not Support Speech Recognition.</div>;
+  }
+  const handleListing = () => {
+    setIsListening(true);
+    microphoneRef.current!.classList.add("listening");
+    SpeechRecognition.startListening({
+      continuous: true
+    });
+  };
+  const stopHandle = () => {
+    setIsListening(false);
+    microphoneRef.current!.classList.remove("listening");
+    SpeechRecognition.stopListening();
+  };
+  const handleReset = () => {
+    stopHandle();
+    resetTranscript();
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="microphone-wrapper">
+        <div className="mircophone-container">
+          <div className="microphone-icon-container" ref={microphoneRef} onClick={handleListing}>
+            <img src={microPhoneIcon} className="microphone-icon" />
+          </div>
+          <div className="microphone-status">{isListening ? "Listening........." : "Click to start Listening"}</div>
+          {isListening && (
+            <button className="microphone-stop btn" onClick={stopHandle}>
+              Stop
+            </button>
+          )}
+        </div>
+        {transcript && (
+          <div className="microphone-result-container">
+            <div className="microphone-result-text">{transcript}</div>
+            <button className="microphone-reset btn" onClick={handleReset}>
+              Reset
+            </button>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
